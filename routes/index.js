d12a0b5c0658c8f275045d99ee7fd0a7f945b6ce
jaceruth <jacemruth@gmail.com>
2023-03-31 12:27:30 -0500

    change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Diployment Lab' });
 });
 
 module.exports = router;
